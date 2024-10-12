746a58b2a5a159a7cda6a455664742a5174e9bdf
yuipercent <154890576+yuipercent@users.noreply.github.com>
2024-10-12 20:46:59 +0200

    Add files via upload

--- core/alkclib.h
+++ core/alkclib.h
@@ -49,7 +49,27 @@
 	 *  Defines the EVENTMAPPING to map an XEvent with a	\
 	 *  type identifier of type "int" with an amount of 6	\
 	 *  events at total.					\
-	 *  							*/
+	 *  							\
+	 * - - - - - - - - - - - - - - - - - - - - - - - - - - -\
+	 * The macro defines the following:			\
+	 *							\
+	 * -> struct ALKISCHEM_EVENTMAPPING			\
+	 *  	# A mapping of events, links the event to	\
+	 *  	# the extractor, AKA the function by which	\
+	 *  	# an inputted event is converted into an ALKORE	\
+	 *  	# compatible event.				\
+	 * 	# Event of index x is linked to the extractor	\
+	 * 	# of index x, for example if KEYS[0] is the	\
+	 * 	# mouseclick event, function EXTRACTOR[0] is	\
+	 * 	# executed on KEYS[0]				\
+	 * 							\
+	 * -> function ALKCEVENT_INITMAP:			\
+	 *  	# Returns an initialized Event mapping		\
+	 *  							\
+	 * -> function ALKCEVENT_MAP:				\
+	 *  	# Assigns the event of identifier CEventID	\
+	 *  	# to the extractor				\
+	 *-----------------------------------------------------*/
 
 #define ALKCDEFINE_WDPACKAGE(WDClass,initfc,openfc,loopfc)	\
 								\
@@ -74,3 +94,26 @@
 		return wd.running;				\
 	};							\
 
+	/**------------------------------------------------------
+	 * ALKCDEFINE_WDPACKAGE DOCUMENTATION :		\
+	 * ------------------------------------------------------
+	 *  Maps the OS-specific functions to the ALkylation	\
+	 *  Compatibility Library functions			\
+	 *							\
+	 * Arguments are as follows:				\
+	 * -> WDClass						\
+	 *  	# Insert here the structure used to hold datas	\
+	 * -> initfc						\
+	 *  	# Insert here the function to instantiante	\
+	 *  	# an object of WDClass				\
+	 * -> openfc						\
+	 *  	# Insert here the function used to open		\
+	 *  	# the window					\
+	 * -> loopfc						\
+	 *  	# Insert here the function used to check wether	\
+	 *  	# the main loop is still running or not		\
+	 *							\
+	 * All functions inputed here should be			\
+	 * extern inline functions				\
+	 * -----------------------------------------------------\
+*/
