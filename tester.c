080cd385365b1898f55c756147292a167b2c035d
yuipercent <154890576+yuipercent@users.noreply.github.com>
2024-10-13 22:34:41 +0200

    Add files via upload

--- tester.c
+++ tester.c
@@ -2,7 +2,7 @@
 #include "alkylfw.h"
 
 int main() {
-	ALKC_STDWD wd = ALKC_INITWD(250,480);
+	ALKC_STDWD* wd = ALKC_INITWD(250,480);
 	ALKC_OPENWD(wd);
 	napht test = VTINIT_naphtha(250,5,RGB);
 	while (ALKC_LOOPWD(wd)) {
