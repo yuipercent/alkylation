a5987a5ef243e22f27e27a3649398b074363684e
yuipercent <154890576+yuipercent@users.noreply.github.com>
2024-10-13 22:35:06 +0200

    Add files via upload

--- srclib/alkc_x11.h
+++ srclib/alkc_x11.h
@@ -12,26 +12,26 @@ typedef struct WDHOLDER{
 	int screen;
 }WDHOLDER;
 
-extern inline WDHOLDER INITWD(int dx, int dy) {
-        WDHOLDER wd;
-        wd.display = XOpenDisplay(NULL);
-        wd.screen = DefaultScreen(wd.display);
-        wd.WD = XCreateSimpleWindow(wd.display,RootWindow(
-                                wd.display,wd.screen), 10, 10,
+extern inline WDHOLDER* INITWD(int dx, int dy) {
+	WDHOLDER* wd = (WDHOLDER*)malloc(sizeof(WDHOLDER));
+        wd->display = XOpenDisplay(NULL);
+        wd->screen = DefaultScreen(wd->display);
+        wd->WD = XCreateSimpleWindow(wd->display,RootWindow(
+                                wd->display,wd->screen), 10, 10,
                                 dx, dy, 1,
-                                BlackPixel(wd.display,wd.screen),
-                                WhitePixel(wd.display,wd.screen));
+                                BlackPixel(wd->display,wd->screen),
+                                WhitePixel(wd->display,wd->screen));
         return wd;
 };
 
-extern inline void OPENWD(WDHOLDER wd) {
-        XMapWindow(wd.display,wd.WD);
-        XSelectInput(wd.display, wd.WD, ExposureMask | KeyPressMask);
+extern inline void OPENWD(WDHOLDER* wd) {
+        XMapWindow(wd->display,wd->WD);
+        XSelectInput(wd->display, wd->WD, ExposureMask | KeyPressMask);
         return;
 };
 
-extern inline void EXTRACTEVENTS(WDHOLDER wd) {
-	XNextEvent(wd.display, &wd.event);
+extern inline void EXTRACTEVENTS(WDHOLDER* wd) {
+	XNextEvent(wd->display, &wd->event);
 };
 
 ALKCDEFINE_WDPACKAGE(WDHOLDER,INITWD,OPENWD,EXTRACTEVENTS);
