c2c6e8ada1de3540b0cf6ef82c7fb208abd417e0
yuipercent <154890576+yuipercent@users.noreply.github.com>
2024-10-27 21:11:01 +0100

    Add files via upload

--- alkylfw.h
+++ alkylfw.h
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 #define ALKC_FRONTENDMODE
 // Used to know wether alkore.h is being defined
 // in frontend or backend to avoid conflict
@@ -7,15 +9,44 @@
 #include "core/alkore.h"
 #endif
 
-#define AlkcWD ALKC_STDWD
+// ====================================================
+#include "plug/naphtools.h"
+
+#define STDWD  ALKC_STDWD
 #define InitWD ALKC_INITWD
 #define OpenWD ALKC_OPENWD
 #define LoopWD ALKC_LOOPWD
+#define naphtFORMAT	VTINIT_naphtFORMAT
+#define naphtha		VTINIT_naphtha
 
 #define napht VT_naphtha
 
-enum ALKYLcompositeSCHEMS{
-	RGBA32 = VTINIT_compositeFORMAT(8,8,32,0),
-	RGB = VTINIT_compositeFORMAT(8,0,24,0)
-}ALKYLcompositeSCHEMS;
+#define RGB ((const struct compositeFORMAT) {	\
+		.BPCA = 8,			\
+		.BFAA = 0,			\
+		.Size = 3,			\
+		.padding = 0,			\
+		.GCID = 128			\
+		})
 
+// RGB32 : 8 bit per color, 0 for alpha, 8 padding for 4 bytes
+#define RGB32 ((const struct compositeFORMAT) {	\
+		.BPCA = 8,			\
+		.BFAA = 0,			\
+		.Size = 4,			\
+		.padding = 8,			\
+		.GCID = 128			\
+		})
+
+#define RGBA32 ((const struct compositeFORMAT) {\
+		.BPCA = 8,			\
+		.BFAA = 8,			\
+		.Size = 4,			\
+		.padding = 0,			\
+		.GCID = 192			\
+		})
+
+enum ALKYLWDconfigs{
+	ALKCDEFAULT = 34493962240,
+	ALKCDEFAULTALPHA = 34494488576
+}ALKYLWDconfigs;
--- tester.c
+++ tester.c
@@ -1,12 +1,22 @@
 #include "srclib/alkc_x11.h"
 #include "alkylfw.h"
+#include <immintrin.h>
+#include <xmmintrin.h>
+#include <emmintrin.h>
 
 int main() {
-	ALKC_STDWD* wd = ALKC_INITWD(250,480);
+	ALKC_STDWD* wd = ALKC_INITWD(250,480,RGB32);
+	VT_naphTestLock(wd->Buffer);
+	//naphtFORMAT(wd->Buffer,RGB,8);
 	ALKC_OPENWD(wd);
-	napht* test = VTINIT_naphtha(250,5,RGB);
+	naphtRect rect;
+	rect.PX = 0;
+	rect.PY = 0;
+	rect.DX = 20;
+	rect.DY = 20;
+	//naphTools_FILLRECT(wd->Buffer,rect,1194876);
+	VT_naphTestLock(wd->Buffer);
 	while (ALKC_LOOPWD(wd)) {
-		
 	};
 	return 0;
 };
