38326868d41bf9f836bdd60424eb7a7bb21520e0
yuipercent <154890576+yuipercent@users.noreply.github.com>
2024-10-28 01:49:16 +0100

    Add files via upload

--- tester.c
+++ tester.c
@@ -5,18 +5,23 @@
 #include <emmintrin.h>
 
 int main() {
-	ALKC_STDWD* wd = ALKC_INITWD(250,480,RGB32);
-	VT_naphTestLock(wd->Buffer);
+	ALKC_STDWD* wd = ALKC_INITWD(250,400,RGB32);
 	//naphtFORMAT(wd->Buffer,RGB,8);
 	ALKC_OPENWD(wd);
 	naphtRect rect;
-	rect.PX = 0;
-	rect.PY = 0;
-	rect.DX = 20;
-	rect.DY = 20;
+	naphtRect clearance;
+	clearance.PX = 0;
+	clearance.PY = 0;
+	clearance.DX = 150;
+	clearance.DY = 150;
+	rect.PX = 10;
+	rect.PY = 20;
 	//naphTools_FILLRECT(wd->Buffer,rect,1194876);
-	VT_naphTestLock(wd->Buffer);
 	while (ALKC_LOOPWD(wd)) {
+		rect.DX = 40;
+		rect.DY = 40;
+		//naphTools_FILLRECT(&wd->attr,clearance,948410);
+		naphTools_FILLRECT(&wd->attr,rect,640);
 	};
 	return 0;
 };
