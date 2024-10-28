f8ba5ea420b33adcf92517a45af826b71150b3ce
yuipercent <154890576+yuipercent@users.noreply.github.com>
2024-10-28 01:50:03 +0100

    Add files via upload

--- plug/naphtools.h
+++ plug/naphtools.h
@@ -15,3 +15,8 @@ typedef struct NaphToolsARG_Rectangle{
 
 static inline _ALKRS_REGTYPEI _NAPHTOOLS_REPEATPAT(long Color,int Size);
 
+static inline void _NAPHTOOLSFILL_REGULAR(WDinfo* wd, naphtRect Args, uint64_t color);
+
+static inline void _NAPHTOOLSFILL_SIMDREGULAR(WDinfo* wd, naphtRect Args, uint64_t color);
+
+extern void naphTools_FILLRECT(WDinfo* wd,naphtRect Args, uint64_t color);
